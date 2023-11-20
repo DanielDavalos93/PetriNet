9dfe677104c22ef0ea1fbe38e7aca8153864b688
DanielDavalos93 <dadaniel.1593@gmail.com>
2023-11-20 12:23:56 -0300

    def redes reversibles

---
+++ PetriNet/Reversibles.lean
@@ -0,0 +1,18 @@
+import PetriNet.Definitions
+import PetriNet.Occurrence
+
+variable (α β : Type)
+
+structure revPetriNet (α : Type) (β : Type) extends (PetriNet α β) where
+  rev_rel_tp : transition →  places → Prop 
+  rev_rel_pt : places →  transition → Prop
+
+ 
+def rev_preset_p {R : revPetriNet α β} (p : R.places) : Set R.transition :=
+  Relation.pre_image R.rel_tp p ∪ Relation.pre_image R.rev_rel_tp p
+
+ 
+def rev_poset_p {R : revPetriNet α β} (p : R.places) : Set R.transition :=
+  Relation.image R.rel_pt p ∪ Relation.image R.rev_rel_pt p
+
+theorem rev_commutative {R : revPetriNet α β} (s s' : Set R.places) (t : R.transition)  (hf : s[*]s') : s'[*]s := by sorry 
