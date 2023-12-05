06a74ef4573b13038359faf9a916277fbd17c878
DanielDavalos93 <dadaniel.1593@gmail.com>
2023-12-05 10:03:23 -0300

    Reversible structure

--- PetriNet/Reversibles.lean
+++ PetriNet/Reversibles.lean
@@ -4,14 +4,12 @@ import PetriNet.Occurrence
 variable (α β : Type)
 
 structure revPetriNet (α : Type) (β : Type) extends (PetriNet α β) where
-  rev_rel_tp : transition →  places → Prop 
-  rev_rel_pt : places →  transition → Prop
-
+  rev_rel_pt (p : places) (t : transition) : Prop := rel_tp t p
+  rev_rel_tp (t : transition) (p : places) : Prop := rel_pt p t
  
 def rev_preset_p {R : revPetriNet α β} (p : R.places) : Set R.transition :=
   Relation.pre_image R.rel_tp p ∪ Relation.pre_image R.rev_rel_tp p
 
- 
 def rev_poset_p {R : revPetriNet α β} (p : R.places) : Set R.transition :=
   Relation.image R.rel_pt p ∪ Relation.image R.rev_rel_pt p
 
